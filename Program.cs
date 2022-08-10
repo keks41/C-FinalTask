de9ebc6ff065756a3e66d4ad6a7d38d0ffe7b2ef
geekbrains <igor_sharin@mail.ru>
2022-08-10 19:26:09 +0300

    Проверяем количество элементов не более 3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿int count = 0;
 string[] array = { "Leonid", "78", "12)", "pro" };
 System.Console.WriteLine("[" + string.Join(" , " , array) + "]");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        count++;
+}
